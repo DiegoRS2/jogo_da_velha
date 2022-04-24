0fb27386fa1ea86e94b5b1c61fb4abc98ad428e6
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2022-04-24 16:50:35 -0300

    jogo completo

---
+++ jogo_da_velha/src/main/java/jogoDaVelha.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Diego
+ */
+public class jogoDaVelha extends JFrame {
+ 
+    public static void main(String[] args){
+       jogo Jogo = new jogo();
+       Jogo.setVisible(true);
+    }
+    
+}
